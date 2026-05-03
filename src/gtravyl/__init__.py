c6bae669a4adcfa59659b4ce46d750b07f94de8d
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-05-03 15:24:55 -0700

    Initial code commit

---
+++ src/gtravyl/__init__.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+from heapq import heappush, heappop
+
+import numpy as np
+
+
+def in_bounds(pt: tuple[int, int], dim: tuple[int, int]):
+    """Checks if the candidate point actually exists in the grid."""
+    return 0 <= pt[0] < dim[0] and 0 <= pt[1] < dim[1]
+
+def vn_neighbors(ind: tuple[int, int], 
+                 grid: np.array, seen: set[tuple[int, int]]):
+    """Return the von Neumann neighborhood of a particular cell in the grid.
+    """
+    left = (ind[0] - 1, ind[1])
+    right = (ind[0] + 1, ind[1])
+    up = (ind[0], ind[1] - 1)
+    down = (ind[0], ind[1] + 1)
+    nbs = [left, right, up, down]
+    nbs = filter(lambda x: in_bounds(x, grid.dim) \
+                       and x not in seen \
+                       and grid[x] != 1, nbs)
+    return nbs
+
+def moore_neighbors(ind: tuple[int, int],
+                    grid: np.array, seen: set[tuple[int, int]],
+                    wrap=False):
+    """Return the Moore neighborhood of a particular cell in the grid."""
+    left_tcorner = (ind[0] - 1, ind[1] + 1)
+    left = (ind[0] - 1, ind[1])
+    left_bcorner = (ind[0] - 1, ind[1] - 1)
+    right_tcorner = (ind[0] + 1, ind[1] + 1)
+    right = (ind[0] + 1, ind[1])
+    right_bcorner = (ind[0] + 1, ind[1] - 1)
+    up = (ind[0], ind[1] - 1)
+    down = (ind[0], ind[1] + 1)
+    nbs = [left_tcorner, left, left_bcorner,
+           right_tcorner, right, right_bcorner,
+           up, down]
+    nbs = filter(lambda x: in_bounds(x) \
+                       and x not in seen \
+                       and grid[x] != 1, nbs)
+    return nbs
+
+
+def shortest_path(grid: np.array,
+                  s: tuple[int, int], t: tuple[int, int],
+                  neighbors=vn_neighbors) -> list[tuple[int, int]]:
+    """Find shortest path from `s` to `t` in a given `grid`."""
+    # Keep track of which nodes need to be explored.
+    frontier = []
+    scores = defaultdict(lambda: float('inf')) # candidate -> current 
+                                                     # best score (ultimately,
+                                                     # best score)
+    parents = {} # candidate -> parent (in particular, the parent with the
+                 # shortest path to s.
+    seen = set() # Keeps track of already seen indices.
+    # Push the node (and it's parent)
+    # NOTE s has no parent thus it is set to None.
+    heappush(frontier, (0, s))
+    parents[s] = None
+    while frontier:
+        (score, candidate) = heappop(frontier)
+        if candidate == t:
+            break
+        seen.add(candidate)
+        for nb in neighbors(candidate, grid, seen):
+            if score + 1 < scores[nb]:
+                scores[nb] = score + 1
+                parents[nb] = candidate
+                heappush(frontier, (score + 1, nb)) 
+    else:
+        return []
+    
+    # Reconstruct:
+    path = [candidate]
+    while (parent := parents[candidate]) is not None:
+        path = [parent] + path
+        candidate = parent
+    return path
